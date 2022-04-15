388442b76e0354052b737f36b78ea5ea4592748b
wilburmao620 <103390241+wilburmao620@users.noreply.github.com>
2022-04-15 11:50:02 -0700

    updated 3

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,7 +20,9 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (!markdown.substring(openBracket-1,openBracket).equals("!")) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         
         }
